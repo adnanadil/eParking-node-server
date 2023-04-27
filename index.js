290d895eadc325bce777be2ecbdd2d143e221d80
Adnan Adil <adnanadil529@gmail.com>
2023-04-27 11:09:30 +0300

    servo open through send_message 2.0

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+    socket.broadcast.emit("receive_message", {
+      message: data.message,
+      room: "16",
+    });
+    // socket.to(data.room).emit("receive_message", data);
     // console.log(`Message ${data.message}`);
     /*Commented this out
     socket.broadcast.emit("receive_message", data);
